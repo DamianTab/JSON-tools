332a120b146951c41e4d5d1d30e4318399f514e9
Krzysztof Gabara <gabara.krzysztof@gmail.com>
2019-11-03 20:32:18 +0100

    cleaned up code for sonar, added lombok dependency and refactored logger

--- src/main/java/pl/put/poznan/transformer/rest/TextTransformerController.java
+++ src/main/java/pl/put/poznan/transformer/rest/TextTransformerController.java
@@ -1,4 +1,5 @@
 package pl.put.poznan.transformer.rest;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
@@ -6,33 +7,32 @@ import pl.put.poznan.transformer.logic.TextTransformer;
 
 import java.util.Arrays;
 
-
+@Slf4j
 @RestController
 @RequestMapping("/{text}")
 public class TextTransformerController {
 
-    private static final Logger logger = LoggerFactory.getLogger(TextTransformerController.class);
 
-    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+    @GetMapping(produces = "application/json")
     public String get(@PathVariable String text,
                               @RequestParam(value="transforms", defaultValue="upper,escape") String[] transforms) {
 
         // log the parameters
-        logger.debug(text);
-        logger.debug(Arrays.toString(transforms));
+        log.debug(text);
+        log.debug(Arrays.toString(transforms));
 
         // do the transformation, you should run your logic here, below just a silly example
         TextTransformer transformer = new TextTransformer(transforms);
         return transformer.transform(text);
     }
 
-    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
+    @PostMapping(produces = "application/json")
     public String post(@PathVariable String text,
                       @RequestBody String[] transforms) {
 
         // log the parameters
-        logger.debug(text);
-        logger.debug(Arrays.toString(transforms));
+        log.debug(text);
+        log.debug(Arrays.toString(transforms));
 
         // do the transformation, you should run your logic here, below just a silly example
         TextTransformer transformer = new TextTransformer(transforms);
