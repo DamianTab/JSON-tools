4149d19479b9e27ca221ac56ab74a2b27c23e773
Krzysztof Gabara <gabara.krzysztof@gmail.com>
2019-12-01 19:15:00 +0100

    added decorator, select, ignore

--- src/main/java/pl/put/poznan/transformer/rest/JsonToolsController.java
+++ src/main/java/pl/put/poznan/transformer/rest/JsonToolsController.java
@@ -1,44 +1,57 @@
 package pl.put.poznan.transformer.rest;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
+import pl.put.poznan.transformer.tools.JsonTools;
+import pl.put.poznan.transformer.tools.JsonToolsFilter;
+import pl.put.poznan.transformer.tools.JsonToolsFilterImpl;
 
-import java.io.IOException;
+import java.util.Set;
 
 @Slf4j
 @RestController
 @RequestMapping
 public class JsonToolsController {
 
-    private ObjectMapper mapper;
+    private final JsonToolsFilterImpl jsonFilterTools;
+    private final JsonTools jsonTools;
 
     @Autowired
-    public JsonToolsController(ObjectMapper mapper) {
-        this.mapper = mapper;
+    public JsonToolsController(@Qualifier("JsonTools") JsonTools jsonTools) {
+        this.jsonTools = jsonTools;
+        jsonFilterTools = new JsonToolsFilterImpl(jsonTools);
     }
 
     @PostMapping(path = "minify", consumes = "application/json")
-    public String minify(@RequestBody String json) throws IOException {
-
-        // log the parameters
-        log.debug("minify called with parameters {}", json);
-        JsonNode jsonNode = mapper.readTree(json);
+    public String minify(@RequestBody String json) {
+        log.debug("minify called with params: JSON: {}", json);
+        JsonNode jsonNode = jsonTools.parseJson(json);
         return jsonNode.toString();
     }
 
     @PostMapping(path = "deminify", produces = "application/json")
-    public JsonNode deminify(@RequestBody String json) throws IOException {
-
-        // log the parameters
-        log.debug("deminify called with parameters {}", json);
-
-        // do the transformation, you should run your logic here, below just a silly example
-        return mapper.readTree(json);
+    public JsonNode deminify(@RequestBody String json) {
+        log.debug("deminify called with params: JSON: {}", json);
+        return jsonTools.parseJson(json);
     }
 
+    @PostMapping(path = "ignore", produces = "application/json")
+    public JsonNode ignore(@RequestBody String json, @RequestParam Set<String> ignored) {
+        log.debug("ignore called with params: JSON: {}, ignoreSet: {}", json, ignored);
+        JsonNode jsonNode = jsonTools.parseJson(json);
+        return jsonFilterTools.ignore(jsonNode, ignored);
+    }
+
+    @PostMapping(path = "select", produces = "application/json")
+    public JsonNode select(@RequestBody String json, @RequestParam Set<String> selected) {
+        log.debug("select called with params: JSON: {}, selected: {}", json, selected);
+        JsonNode jsonNode = jsonTools.parseJson(json);
+        return jsonFilterTools.select(jsonNode, selected);
+    }
 
 }
 
---
+++ src/main/java/pl/put/poznan/transformer/tools/JsonToolsFilterImpl.java
@@ -0,0 +1,35 @@
+package pl.put.poznan.transformer.tools;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class JsonToolsFilterImpl implements JsonToolsFilter {
+
+    private JsonTools jsonTools;
+
+    public JsonToolsFilterImpl(JsonTools jsonTools) {
+        this.jsonTools = jsonTools;
+    }
+
+    @Override
+    public JsonNode parseJson(String json) {
+        return jsonTools.parseJson(json);
+    }
+
+    public JsonNode ignore(JsonNode json, Set<String> ignored) {
+        ObjectNode objectNode = (ObjectNode) json;
+        ignored.forEach(property -> objectNode.findParent(property).without(property));
+        return objectNode;
+    }
+
+    public JsonNode select(JsonNode json, Set<String> selected) {
+        ObjectNode objectNode = (ObjectNode) json;
+        selected.forEach(property -> objectNode.findParent(property).retain(property));
+        return objectNode;
+    }
+}
